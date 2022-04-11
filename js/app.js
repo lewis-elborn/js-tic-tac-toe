2014445b3a162d8d69afd16f719674fb3a70f967
Lewis Elborn <lewis.elborn@aphinity.co.uk>
2022-04-11 15:30:24 +0100

    Added Package Data & Additional Css.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-window.onload = function() {
-    gameState();
-};
+window.onload = function () {
+    gameState()
+}
+/**
+ * Adds two numbers together.
+ * @param {int} num1 The first number.
+ */
 function gameState() {
-    console.log("Hello! I am a console log!!");
+    console.log('Hello! I am a console log!!')
 }
